1b22ac9220e970da3df21084a3ea58ed258ec0f9
Hugo Solares <Hugo.Solares@jala.university>
2023-01-05 20:44:39 -0400

    First test in Rest Assured

--- src/test/java/testone.java
+++ src/test/java/testone.java
@@ -1,17 +1,23 @@
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
-public class testone {
+public class Test_one {
 
     @Test
     public void test_01() {
-        RestAssured.baseURI = "https://test-api.k6.io/auth/basic/login/";
-        RestAssured.given().body("{\n" +
-                        "    \"username\": \"hugo96sa\",\n" +
-                        "    \"password\": \"73223294\"\n" +
-                        "}").post().then()
-                .log()
-                .all()
+        RestAssured.baseURI = "https://test-api.k6.io";
+        JSONObject bodyParams = new JSONObject();
+        bodyParams.put("username", "hugo96sa");
+        bodyParams.put("password", "73223294");
+
+        RestAssured.given()
+                .body(bodyParams.toString()).contentType(ContentType.JSON)
+                .when()
+                .post("/auth/basic/login/")
+                .then()
+                .log().all()
                 .assertThat()
                 .statusCode(200);
     }
